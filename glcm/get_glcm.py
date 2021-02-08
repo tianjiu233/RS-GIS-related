1d3358974f07ddd44c069eab711f260b5ff975b7
tianjiu233 <huijianpzh@gmail.com>
2021-02-08 14:19:08 +0800

    add some codes

--- glcm/get_glcm.py
+++ glcm/get_glcm.py
@@ -4,6 +4,14 @@ Created on Fri Jan 29 10:33:23 2021
 
 @author: huijian
 """
+
+"""
+Reference:
+    https://zhuanlan.zhihu.com/p/110228230
+    http://murphylab.web.cmu.edu/publications/boland/boland_node26.html
+    https://prism.ucalgary.ca/handle/1880/51900
+    https://github.com/LinghuiXia/GLCM
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
@@ -11,6 +19,8 @@ from skimage import data,io
 from math import floor,ceil
 from skimage.feature import greycomatrix,greycoprops
 from skimage.color import rgb2gray
+
+from matplotlib import pyplot as plt
 
 def image_patch(image,slide_window,h,w):
     
@@ -60,7 +70,7 @@ def calcu_glcm_mean(glcm,nbit=64):
 
 def calcu_glcm_variance(glcm,nbit=64):
     
-    mean = np.zeros(glcm.shape[2],glcm.shape[3],dtype=np.float32)
+    mean = np.zeros((glcm.shape[2],glcm.shape[3]),dtype=np.float32)
     for i in range(nbit):
         for j in range(nbit):
             mean += glcm[i,j]*i/(nbit)**2
@@ -69,7 +79,7 @@ def calcu_glcm_variance(glcm,nbit=64):
         for j in range(nbit):
             variance += glcm[i,j] * (i-mean)**2
             
-    return
+    return variance
 
 def calcu_glcm_homogeneity(glcm,nbit=64):
     
@@ -91,12 +101,21 @@ def calcu_glcm_contrast(glcm,nbit=64):
 
 def calcu_glcm_dissimilarity(glcm,nbit=64):
     
-    dissimilarty = np.zeors((glcm.shape[2],glcm.shape[3]),dtype=np.float32)
+    dissimilarty = np.zeros((glcm.shape[2],glcm.shape[3]),dtype=np.float32)
     for i in range(nbit):
         for j in range(nbit):
             dissimilarty += glcm[i,j] * np.abs(i-j)
             
     return dissimilarty
+
+def calcu_glcm_entropy(glcm, nbit=64):
+    eps = 0.00001
+    entropy = np.zeros((glcm.shape[2], glcm.shape[3]), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            entropy -= glcm[i, j] * np.log10(glcm[i, j] + eps)
+
+    return entropy
 
 def calcu_glcm_energy(glcm, nbit=64):
 
@@ -161,10 +180,88 @@ if __name__=="__main__":
     glcm = calcu_glcm(image,min_,max_,nbit,slide_window,step,angle)
     
     # ------ calculate the feature ------
-    
-    
+    for i in range(glcm.shape[2]):
+        for j in range(glcm.shape[3]):
+            glcm_slice = np.zeros((nbit,nbit,h,w),dtype=np.float32)
+            # glcm_slice: [nbit,nbit,h,w]
+            glcm_slice = glcm[:,:,i,j,:,:]
+            
+            mean = calcu_glcm_mean(glcm_slice,nbit)
+            variance = calcu_glcm_variance(glcm_slice,nbit)
+            homogeneity = calcu_glcm_homogeneity(glcm_slice,nbit)
+            contrast = calcu_glcm_contrast(glcm_slice,nbit)
+            dissimilarity = calcu_glcm_dissimilarity(glcm_slice,nbit)
+            entropy = calcu_glcm_entropy(glcm_slice,nbit)
+            energy = calcu_glcm_energy(glcm_slice,nbit)
+            correlation = calcu_glcm_correlation(glcm_slice,nbit)
+            auto_correlation = calcu_glcm_auto_correlation(glcm_slice,nbit)
+            
     # ------ visulization ------
-    #plt.subplot(2,5,1)
+    
+    font = {"family":"Times New Roman",
+            "weight":"normal",
+            "size":12,}
+    
+    plt.subplot(2,5,1)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.axis("off")
+    plt.imshow(image,cmap="gray")
+    plt.title("Original",font)
+    
+    plt.subplot(2,5,2)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.axis("off")
+    plt.imshow(mean,cmap="gray")
+    plt.title("Mean",font)
+    
+    plt.subplot(2,5,3)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.axis("off")
+    plt.imshow(variance,cmap="gray")
+    plt.title("Variance",font)
+    
+    plt.subplot(2,5,4)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.axis("off")
+    plt.imshow(homogeneity,cmap="gray")
+    plt.title("Homogeneity",font)
+    
+    plt.subplot(2,5,5)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.axis("off")
+    plt.imshow(contrast,cmap="gray")
+    plt.title("Contrast",font)
+    
+    plt.subplot(2,5,6)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.axis("off")
+    plt.imshow(dissimilarity,cmap="gray")
+    plt.title("Dissimilarity",font)
+    
+    plt.subplot(2,5,7)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.axis("off")
+    plt.imshow(entropy,cmap="gray")
+    plt.title("Entropy",font)
+    
+    plt.subplot(2,5,8)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.axis("off")
+    plt.imshow(energy,cmap="gray")
+    plt.title("Energy",font)
+    
+    plt.subplot(2,5,9)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.axis("off")
+    plt.imshow(correlation,cmap="gray")
+    plt.title("Correlation",font)
+    
+    plt.subplot(2,5,10)
+    plt.tick_params(labelbottom=False,labelleft=False)
+    plt.axis("off")
+    plt.imshow(auto_correlation,cmap="gray")
+    plt.title("Auto_correlation",font)
+    
     
     
     
